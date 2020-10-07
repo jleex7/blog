2ea2cf47e98127c00fa051f27c4cd2be84d53036
John Lee <jleex89@gmail.com>
2020-10-07 13:38:18 -0400

    fixed_build.py

--- build.py
+++ build.py
@@ -4,26 +4,30 @@
 top_html = open('templates/top.html').read()
 bottom_html = open('templates/bottom.html').read()
 index_html = open('content/index.html').read()
-index.html = top_html + index_html + bottom_html
-print(index.html)
+combined_index = top_html + index_html + bottom_html
+print(combined_index)
+open('docs/index.html', 'w+').write(combined_index)
 
 # Python Script for TechNews.Html
 top_html = open('templates/top.html').read()
 bottom_html = open('templates/bottom.html').read()
 technews_html = open('content/technews.html').read()
-index.html = top_html + technews_html + bottom_html
-print(technews.html)
+combined_technews = top_html + technews_html + bottom_html
+print(combined_technews)
+open('docs/technews.html', 'w+').write(combined_technews)
 
 # Python Script for Portfolio.Html
 top_html = open('templates/top.html').read()
 bottom_html = open('templates/bottom.html').read()
 portfolio_html = open('content/portfolio.html').read()
-index.html = top_html + portfolio_html + bottom_html
-print(portfolio.html)
+combined_portfolio = top_html + portfolio_html + bottom_html
+print(combined_portfolio)
+open('docs/portfolio.html', 'w+').write(combined_portfolio)
 
 # Python Script for Contact.Html
 top_html = open('templates/top.html').read()
 bottom_html = open('templates/bottom.html').read()
 contact_html = open('content/contact.html').read()
-index.html = top_html + contact_html + bottom_html
-print(content.html)
+combined_contact = top_html + contact_html + bottom_html
+print(combined_contact)
+open('docs/contact.html', 'w+').write(combined_contact)
