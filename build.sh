422fa3e3c55eeffb0440a0226633869cdc072a18
John Lee <jleex89@gmail.com>
2020-10-06 23:26:14 -0400

    updated_buid.sh

--- build.sh
+++ build.sh
@@ -3,11 +3,11 @@
 # Bash Script for Index.Html
 cat templates/top.html content/index.html templates/bottom.html > docs/index.html
 
-# Bash Script for Index2.Html
+# Bash Script for TechNews.Html
 cat templates/top.html content/technews.html templates/bottom.html > docs/technews.html
 
-# Bash Script for Index3.Html
+# Bash Script for Portfolio.Html
 cat templates/top.html content/portfolio.html templates/bottom.html > docs/portfolio.html
 
-# Bash Script for Index4.Html
+# Bash Script for Contact.Html
 cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
