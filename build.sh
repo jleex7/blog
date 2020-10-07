5ba218a5cebcc75ad05530bd6dd1ae2004d1917c
John Lee <jleex89@gmail.com>
2020-10-06 21:34:14 -0400

    created_bashscript_and_redirected_files

---
+++ build.sh
@@ -0,0 +1,13 @@
+# Building A Bash Script
+
+# Bash Script for Index.Html
+cat templates/top.html content/index.html templates/bottom.html > docs/combinedindex.html
+
+# Bash Script for Index2.Html
+cat templates/top.html content/index2.html templates/bottom.html > docs/combinedindex2.html
+
+# Bash Script for Index3.Html
+cat templates/top.html content/index3.html templates/bottom.html > docs/combinedindex3.html
+
+# Bash Script for Index4.Html
+cat templates/top.html content/index4.html templates/bottom.html > docs/combinedindex4.html
