74003d783545b0a950639127462cddc3ec855dcf
John Lee <jleex89@gmail.com>
2020-10-06 22:49:33 -0400

    fixed_filenames

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 # Building A Bash Script
 
 # Bash Script for Index.Html
-cat templates/top.html content/index.html templates/bottom.html > docs/combinedindex.html
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
 
 # Bash Script for Index2.Html
-cat templates/top.html content/index2.html templates/bottom.html > docs/combinedindex2.html
+cat templates/top.html content/technews.html templates/bottom.html > docs/technews.html
 
 # Bash Script for Index3.Html
-cat templates/top.html content/index3.html templates/bottom.html > docs/combinedindex3.html
+cat templates/top.html content/portfolio.html templates/bottom.html > docs/portfolio.html
 
 # Bash Script for Index4.Html
-cat templates/top.html content/index4.html templates/bottom.html > docs/combinedindex4.html
+cat templates/top.html content/contact.html templates/bottom.html > docs/contact.html
