3f8af911f21ee73387538718fc929fc378366f44
John Lee <jleex89@gmail.com>
2020-10-17 18:58:10 -0400

    deleted_unnecessary_files

--- build.py
+++ build.py
@@ -1,33 +1,44 @@
 # Building A Python Script
 
-# Python Script for Index.Html
-top_html = open('templates/top.html').read()
-bottom_html = open('templates/bottom.html').read()
-index_html = open('content/index.html').read()
-combined_index = top_html + index_html + bottom_html
-print(combined_index)
-open('docs/index.html', 'w+').write(combined_index)
+#Using Main Function
+def Main ()
+	# Python Script for Index.Html
+	top_html = open('templates/top.html').read()
+	bottom_html = open('templates/bottom.html').read()
+	index_html = open('content/index.html').read()
+	combined_index = top_html + index_html + bottom_html
+	print(combined_index)
+	open('docs/index.html', 'w+').write(combined_index)
 
-# Python Script for TechNews.Html
-top_html = open('templates/top.html').read()
-bottom_html = open('templates/bottom.html').read()
-technews_html = open('content/technews.html').read()
-combined_technews = top_html + technews_html + bottom_html
-print(combined_technews)
-open('docs/technews.html', 'w+').write(combined_technews)
+	# Python Script for TechNews.Html
+	top_html = open('templates/top.html').read()
+	bottom_html = open('templates/bottom.html').read()
+	technews_html = open('content/technews.html').read()
+	combined_technews = top_html + technews_html + bottom_html
+	print(combined_technews)
+	open('docs/technews.html', 'w+').write(combined_technews)
 
-# Python Script for Portfolio.Html
-top_html = open('templates/top.html').read()
-bottom_html = open('templates/bottom.html').read()
-portfolio_html = open('content/portfolio.html').read()
-combined_portfolio = top_html + portfolio_html + bottom_html
-print(combined_portfolio)
-open('docs/portfolio.html', 'w+').write(combined_portfolio)
+	# Python Script for Portfolio.Html
+	top_html = open('templates/top.html').read()
+	bottom_html = open('templates/bottom.html').read()
+	portfolio_html = open('content/portfolio.html').read()
+	combined_portfolio = top_html + portfolio_html + bottom_html
+	print(combined_portfolio)
+	open('docs/portfolio.html', 'w+').write(combined_portfolio)
 
-# Python Script for Contact.Html
-top_html = open('templates/top.html').read()
-bottom_html = open('templates/bottom.html').read()
-contact_html = open('content/contact.html').read()
-combined_contact = top_html + contact_html + bottom_html
-print(combined_contact)
-open('docs/contact.html', 'w+').write(combined_contact)
+	# Python Script for Contact.Html
+	top_html = open('templates/top.html').read()
+	bottom_html = open('templates/bottom.html').read()
+	contact_html = open('content/contact.html').read()
+	combined_contact = top_html + contact_html + bottom_html
+	print(combined_contact)
+	open('docs/contact.html', 'w+').write(combined_contact)
+main()
+
+pages = [
+	{
+		'filename': 
+		'output': 
+		'title': index
+	}
+	
