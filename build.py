7b338bff25379fa048904e5b685e2bb788a32e5a
John Lee <jleex89@gmail.com>
2020-10-19 20:30:26 -0400

    updated_build.py

--- build.py
+++ build.py
@@ -1,3 +1,20 @@
+def apply_template(filename, page_title):
+	#opens base template
+	base = open('templates/base.html').read()
+	#update title in base template page with title from list
+	title_content = base.replace('{{title}}', page_title)
+	#opens content files [look at loops]
+	content = open(filename).read()
+	#combine base template + content pages
+	combined_content = title_content.replace('{{content}}', content)
+	print(combined_content)
+	#returns combined_content
+	return combined_content
+
+def write_outputpage(outputFilename, combined_page):
+	#opens updated content file and write it into docs folder
+	open(outputFilename, 'w+').write(combined_page)
+
 def main():
 
 	# creates a list of directories called pages
@@ -24,28 +41,11 @@ def main():
 			},
 			]
 
-	def apply_template(filename):
-		#opens base template
-		base = open('templates/base.html').read()
-		#update title in base template page
-		title_content = base.replace('{{title}}', page_title)
-		#opens content files [look at loops]
-		content = open(filename).read()
-		#combine base template + content pages
-		combined_content = base.replace('{{content}}', content)
-		print(combined_content)
-		#returns combined_content
-		return combined_content
 
-	def write_outputpage(outputFilename, combined_page):
-		#opens updated content file and write it into docs folder
-		open(outputFilename, 'w+').write(combined_page)
 
     # runs for loop for index/tech_news/portfolio/contact in pages and writes the files in doc folder
 	for page in pages:
-		page_title = page['title']
-		print(page_title)
-		combined_page = apply_template(page['filename'])
+		combined_page = apply_template(page['filename'], page['title'])
 		print(combined_page)
 		write_outputpage(page['output'], combined_page)
 
